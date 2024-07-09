6c53de258205067cf4f78345b4b42781b39c16a9
DaniTech <tochidan11@gmail.com>
2024-07-09 02:28:28 -0700

    wrote the deploy function in the deploy.js file.

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,26 +6,50 @@
 // global scope, and execute the script.
 const hre = require("hardhat");
 
+// console.log(hre);
+
+// async function main() {
+//   const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+//   const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
+//   const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+
+//   const lockedAmount = hre.ethers.utils.parseEther("1");
+
+//   const Lock = await hre.ethers.getContractFactory("Lock");
+//   const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+
+//   await lock.deployed();
+
+//   console.log(
+//     `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+//   );
+// }
+
+// // We recommend this pattern to be able to use async/await everywhere
+// // and properly handle errors.
+// main().catch((error) => {
+//   console.error(error);
+//   process.exitCode = 1;
+// });
+
 async function main() {
+  //Note timestamp in blockchain comes in seconds, so we need to convert our own time to seconds
   const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  const ONE_YEAR_IN_SECS = 356 * 24 * 60 * 60;
+
+  const unlockedTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
 
   const lockedAmount = hre.ethers.utils.parseEther("1");
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  // console.log(currentTimestampInSeconds);
+  // console.log(ONE_YEAR_IN_SECS);
+  // console.log(unlockedTime);
+  // console.log(lockedAmount);
 
-  await lock.deployed();
-
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  const MyTest
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
-  console.error(error);
+  console.log(error);
   process.exitCode = 1;
 });
